2d8099a0c20217dc270bb3b17d40db36168310e8
Courtney Woods <cswoods88@gmail.com>
2016-09-01 17:09:46 -0600

    think shes done

--- app.js
+++ app.js
@@ -2,35 +2,27 @@
 
 $(document).ready(function() {
     // map
-    L.mapbox.accessToken = 'pk.eyJ1IjoiY3N3b29kczg4IiwiYSI6ImNpc2kzZW16bjAwMnAzMHB1bG5lNzJ0NWIifQ.qm4p6yO3ABYa_YXYXZNlpg';
-    var map = L.mapbox.map('map', 'mapbox.streets');
-    L.control.locate().addTo(map);
-    
-
+    L.mapbox.accessToken = 'pk.eyJ1IjoiY3N3b29kczg4IiwiYSI6ImNpc2kzZW16bjAwMnAzMHB1bG5lNzJ0NWIifQ.qm4p6yO3ABYa_YXYXZNlpg'
+    var map = L.mapbox.map('map', 'mapbox.streets')
+    L.control.locate().addTo(map)
 
     // Search Icon toggles back and forth between maps/weather and parralax
     $('#reSearch').click(function() {
         $('#index-banner').toggle()
-
     })
 
-    // Submit Button
-    // $('.submitButton').click(function(e) {
-    //     e.preventDefault();
-    //     getInfo();
-    // })
+    $('.myInput').keypress(function(event) {
+            if (event.which == 13) {
+                event.preventDefault()
+                getInfo()
+            }
+        })
 
-$('.myInput').keypress(function (event) {
-  if(event.which == 13){
-    event.preventDefault()
-    getInfo()
-  }
-})
-    // Api communication
+        // Api communication
     function getInfo() {
-        var lat;
-        var long;
-        var searchTerm = $('.myInput').val();
+        var lat
+        var long
+        var searchTerm = $('.myInput').val()
         var encoded = encodeURIComponent(searchTerm)
 
         // island.io API
@@ -42,38 +34,22 @@ $('.myInput').keypress(function (event) {
                 if (!data.items.length) {
                     Materialize.toast('Location not found', 3000)
                 }
-                long = data.items[0].location.longitude;
-                lat = data.items[0].location.latitude;
+                long = data.items[0].location.longitude
+                lat = data.items[0].location.latitude
 
                 // Initalizing map and giving access to current location
+                map.setView([lat, long], 9)
+                L.marker([lat, long]).bindPopup(`This is ${searchTerm}`).addTo(map)
 
-                map.setView([lat, long], 9);
-                var location = L.marker([lat, long]).bindPopup(`This is ${searchTerm}`).addTo(map)
-                var weather;
-
-
-
-                // forcast.io API
-                $.ajax({
-                    method: 'GET',
-                    url: `https://api.forecast.io/forecast/809a83b8b5c17d7f929dda132a82066d/${lat},${long}`,
-                    datatype: 'json',
-                    success: function(data) {
-                        console.log(data);
-                        let weather = data.currently;
-                        console.log(data.currently);
-                        $('#index-banner').hide()
-                        $('#weatherDiv').html(`<iframe id="forecast_embed" type="text/html" frameborder="0" height="245" width="100%" src="https://forecast.io/embed/#lat=${lat}&lon=${long}&name=${searchTerm}&color=#00aaff&font=Georgia&units=uk"></iframe>`)
-                    }
-                })
+                // display weather
+                $('#index-banner').hide()
+                $('#weatherDiv').html(`<iframe id="forecast_embed" type="text/html" frameborder="0" height="245" width="100%" src="https://forecast.io/embed/#lat=${lat}&lon=${long}&name=${searchTerm}&color=#00aaff&font=Georgia&units=uk"></iframe>`)
             },
             error: function(err) {
-                console.log('there was an error:', err);
+                console.log('there was an error:', err)
             }
         })
     }
-
-    function getWeather(long, lat) {}
     // parralax
-    $('.parallax').parallax();
-});
+    $('.parallax').parallax()
+})
